eddf7fc83b785f1a7abda45beedf00a5da09966f
Margaret <match14@yandex.ru>
2020-07-12 21:19:46 +0300

    adaptive main

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,30 @@ import {Swiper} from './common';
 
 var mySwiper = new Swiper('.js-services-slider', {
 	loop: true,
-	slidesPerView: 5,
+	slidesPerView: 1,
 	spaceBetween: 46,
 	navigation: {
 		nextEl: '.js-services-next',
 		prevEl: '.js-services-prev',
 	},
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		},
+		1350: {
+			slidesPerView: 4,
+		},
+		1800: {
+			slidesPerView: 5,
+		},
+	  }
 })
 
 var topSlider = new Swiper('.js-top-slider', {
 	loop: true,
-	// slidesPerView: 5,
-	// spaceBetween: 46,
 	pagination: {
 		el: '.js-swiper-pagination',
 	},
